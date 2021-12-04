2e9157fb163275606976315d2c4ef01c69c6df63
yofikingstonp111 <yofikingston@gmail.com>
2021-12-04 08:46:07 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Semua sintaksis  dasar bahasa pemrograman terdiri dari :
+1.Sekuensial:Langkah berurutan
+2.Percabangan:Langkah melompat  jika kondisi terpenuhi
+3.Perulangan:Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# sekuensial
+print('Mom said, "go to store" ' )
+print('Budi answer "ok, what do am i must be   in the store" ' )
+print('Mom said, "buy one egg and  if there are the egg, you can buy egg six"')
+print('Budi answer "ok"' )
+print("So budi go to the store")
+print("To buy some necessary")
+
+#percabangan
+number_of_bottles_of_milk =0
+
+if number_of_bottles_of_milk >0:
+    print("Budi is cheking the price,  and then  his money is enough")
+    print("Budi buy  one milk bottled")
+else:
+    print("Budi does not buy one  milk bottle")
+
+print("andi go to home")
+print("andi convey the result to mother ")
+ 
