bae5ed0fab46a4d1734ed815cdb85db47d20a42f
yofikingstonp111 <yofikingston@gmail.com>
2021-12-04 09:16:25 +0700

    initial commit

--- practice_python_fundamental.py
+++ practice_python_fundamental.py
@@ -1,2 +1,14 @@
-#
-print("saya membuat program baru")
+#branching
+print("Mother gave ordered to the child")
+print("The child go to the staore")
+milk=6000
+egg=1500
+if milk <6000:
+    print("The child is checking to the price")
+    print("The child not buy a milk bottled")
+else:
+    print("The child ddoes not buy milk and egg")
+    print("The child go to home")
+    print("The child submit to her mother")
+
+
