e7e3e19cf7ab74eef5347d6b753b9a0a0c655c7e
yofikingstonp111 <yofikingston@gmail.com>
2021-12-04 10:02:39 +0700

    initial commit

--- practice_python_fundamental.py
+++ practice_python_fundamental.py
@@ -1,13 +1,18 @@
 #branching
 print("Mother gave ordered to the child")
-print("The child go to the staore")
+print("The child go to the store")
+amount_money=10000
 milk=6000
 egg=1500
-if milk <6000:
-    print("The child is checking to the price")
-    print("The child not buy a milk bottled")
-else:
-    print("The child ddoes not buy milk and egg")
+if amount_money > milk:
+    print("The child buys one milk")
+
+
+    if amount_money > egg:
+        print("buy some six egg")
+    else:
+        print("The child does not buy the egg")
+
     print("The child go to home")
     print("The child submit to her mother")
 
