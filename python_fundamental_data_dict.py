2bbc43a1721dfee22aa868d366222a54893d6301
yofikingstonp111 <yofikingston@gmail.com>
2021-12-07 23:34:41 +0700

    python_fundamental_data_dict.py

--- python_fundamental_data_dict.py
+++ python_fundamental_data_dict.py
@@ -1,15 +1,39 @@
 users = {
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': 'Bret',
-    'email': 'Sincere@april.biz',
-
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
 }
 
 print(users)
-print(users['id'])
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"])
+print(users["address"]["geo"]["lat"])
+print(users["address"]["geo"]["lng"])
 
-print("hi")
+# python dictionary and json almost same, but this is different about this.
+# you can using default python function with import
+print(users)
+print(type(users))
+print('\nChange dictionary to json')
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
+
